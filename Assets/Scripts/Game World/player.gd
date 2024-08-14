00b5e0d374d7579d0379b4e3b3b2b9cee702f1c1
DrWaffle454 <bwpond@yahoo.com>
2024-08-13 22:57:50 -0700

    Movement
    Test Dummy Model for Player, Functional base movement script.

---
+++ Assets/Scripts/Game World/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
+	
+	# Normalize the direction to ensure consistent speed in all directions
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+
+	# Apply movement
+	velocity.x = direction.x * SPEED
+	velocity.y = direction.y * SPEED
+
+	move_and_slide()
