a78e038f5437938a70f83829f5411b6fcb2bbd53
DrWaffle454 <bwpond@yahoo.com>
2024-08-13 21:56:14 -0700

    Main Menu Update
    Start and Stop now function properly.

---
+++ Assets/Scripts/Menu UI/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+#Start Button
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Assets/Scenes/CORE/main.tscn")
+	
+	
+
+#Options Button
+func _on_options_button_pressed():
+	pass # Replace with function body.
+	
+
+#Quit button
+func _on_quit_button_pressed():
+	get_tree().quit()
