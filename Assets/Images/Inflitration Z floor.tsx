2bf2bf44c3e27e14eb65408a79a8379017b7635e
DrWaffle454 <bwpond@yahoo.com>
2024-08-13 20:17:38 -0700

    Basic

---
+++ Assets/Images/Inflitration Z floor.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Inflitration Z floor" tilewidth="16" tileheight="16" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image source="../../../Downloads/pixil-frame-0 (2).png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../Downloads/floor-tile-wooden-0-pixilart (1).png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../Downloads/pixil-frame-0 (1).png" width="16" height="16"/>
+ </tile>
+</tileset>
