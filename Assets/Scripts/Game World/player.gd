8502662a82edbf0a93372928fc367ca294a94676
Rook17 <alexbrook2006@outlook.com>
2024-08-14 00:59:48 -0700

    Player script change for movement

--- Assets/Scripts/Game World/player.gd
+++ Assets/Scripts/Game World/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
 func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Vector2(
@@ -9,6 +12,17 @@ func _physics_process(_delta):
 		Input.get_axis("move_up", "move_down")
 	)
 	
+	# Face direction of movement and play correct animations
+	if direction.x != 0:
+		animated_sprite_2d.play("run")
+		if direction.x < 0:
+			animated_sprite_2d.flip_h = true
+		elif direction.x > 0:
+			animated_sprite_2d.flip_h = false
+	else:
+		animated_sprite_2d.play("idle")
+		
+		
 	# Normalize the direction to ensure consistent speed in all directions
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
