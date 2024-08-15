cd190f9c259d98463a0c4091662f25ccd956b0ad
Rook17 <alexbrook2006@outlook.com>
2024-08-15 01:49:47 -0700

    Zombie, random direction

--- Zombie.gd
+++ Zombie.gd
@@ -1,10 +1,10 @@
-extends Node
+extends CharacterBody2D
 
 # ~~~~~~~~~~~~~~~~~ #
 # The constants: Change variables here, not in later code in script #
-const SPEED := 45
-var direction_x := 1
-var direction_y := 1
+const SPEED := 25
+var direction = Vector2.ZERO
+var movement_timer = 5
 # ~~~~~~~~~~~~~~~~~ #
 
 # Variables via children objects
@@ -17,42 +17,37 @@ var direction_y := 1
 
 @onready var timer = $Timer
 
-# Does everything
+func _ready():
+	set_random_direction()
+
+
 func _process(delta):
 	
 	# Faces the right direction and doesn't run into walls
 	if ray_cast_right.is_colliding():
-		direction_x = -1
+		direction = Vector2(-1, 0).normalized()
 		animated_sprite_2d.flip_h = true
 	elif ray_cast_left.is_colliding():
-		direction_x = 1
+		direction = Vector2(1, 0).normalized()
 		animated_sprite_2d.flip_h = false
 	elif ray_cast_down.is_colliding():
-		direction_y = 1
+		direction = Vector2(0, 1).normalized()
 	elif ray_cast_up.is_colliding():
-		direction_y = -1
+		direction = Vector2(0, -1).normalized()
 		
-	# Calls upon the timer to add a delay to calling a function, returns int range [1,2]
-	var direction = timer.start()
-	# 1 == x, 2 == y #
-	
-	# if x axis was chosen
-	if direction == 1:
-		var direction_direction_x = randi_range(-1,1)
-		if direction_direction_x == 1:
-			animated_sprite_2d.position.x += direction_x * SPEED * delta
-		elif direction_direction_x == -1:
-			animated_sprite_2d.position.x += (-1) * (direction_x * SPEED * delta)
+	direction = direction.normalized()
+		
+	movement_timer -= delta
+	if movement_timer <= 0:
+		set_random_direction()
+		movement_timer = 5
 			
-	# if y axis was chosen
-	elif direction == 2:
-		var direction_direction_y = randi_range(-1,1)
-		if direction_direction_y == 1:
-			animated_sprite_2d.position.y += direction_y * SPEED * delta
-		elif direction_direction_y == -1:
-			animated_sprite_2d.position.y += (-1) * (direction_y * SPEED * delta)
+	velocity = direction * SPEED
+	move_and_slide()
 	
 func _on_timer_timeout():
-	# Chooses randomly between x or y
-	var ran_direction = randi_range(1,2)
-	return ran_direction
+	pass
+	
+func set_random_direction():
+	var angle = randf() * 2 * PI
+	direction = Vector2(cos(angle), sin(angle)).normalized()
