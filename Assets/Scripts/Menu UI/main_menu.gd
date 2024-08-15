9ac4ab47921cd8f6826674e461e4842bf97e779f
Rook17 <alexbrook2006@outlook.com>
2024-08-14 21:07:18 -0700

    oihiuh

--- Assets/Scripts/Menu UI/main_menu.gd
+++ Assets/Scripts/Menu UI/main_menu.gd
@@ -1,20 +1,17 @@
 extends Control
 
-func _ready():
-	MainMenuMusic.play_music_level()
 
 #Start Button
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Assets/Scenes/Game World/test_map.tscn")
-	MainMenuMusic.stop_music()
+	get_tree().change_scene_to_file("res://Assets/Scenes/CORE/main.tscn")
+	
 	
 
 #Options Button
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://Assets/Scenes/Menu UI/volume_menu.tscn")
+	pass # Replace with function body.
 	
 
 #Quit button
 func _on_quit_button_pressed():
 	get_tree().quit()
-
