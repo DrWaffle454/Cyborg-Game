3111786f49f2dfa8c8efbb8ab1b2e8b774aba519
DrWaffle454 <bwpond@yahoo.com>
2024-08-14 17:19:36 -0700

    Edited Player and Added Test Map

--- Assets/Scripts/Menu UI/main_menu.gd
+++ Assets/Scripts/Menu UI/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 #Start Button
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Assets/Scenes/CORE/main.tscn")
+	get_tree().change_scene_to_file("res://Assets/Scenes/Game World/test_map.tscn")
 	
 	
 
